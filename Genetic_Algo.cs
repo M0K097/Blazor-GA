babb4fa39638bede71bccc7b19e187fcc8a3b40f
M0K0 <m0k097@gmx.de>
2025-11-15 13:13:53 +0100

    dynamic settings field

--- Genetic_Algo.cs
+++ Genetic_Algo.cs
@@ -1,5 +1,4 @@
-//a simple genetic algorithm in csharps blazor to run in browser
-
+//a simple genetic algorithm in C#'s Blazor to run in browser
 public class Genome
 {
 		public Random dice = new Random();
@@ -9,12 +8,12 @@ public class Genome
 
 		public void init(int dna_length)
 		{
-				for (int x = 0; x < dna_length; x++)
-				{
-						char[] tmp = logos.ToCharArray();
-						var pick = tmp[dice.Next(tmp.Length )];
-						genetic_material.Add(pick);
-				}
+			for (int x = 0; x < dna_length; x++)
+			{
+				char[] tmp = logos.ToCharArray();
+				var pick = tmp[dice.Next(tmp.Length )];
+				genetic_material.Add(pick);
+			}
 		}
 
 		public string get_dna()
@@ -22,6 +21,7 @@ public class Genome
 			string dna = new string(genetic_material.ToArray());
 			return dna;
 		}
+
 		public void mutate(int mutation_rate)
 		{
 			for(int pos = 0; pos < genetic_material.Count()-1;pos++)
@@ -29,34 +29,33 @@ public class Genome
 				var chance = dice.Next(100);
 				if (chance <= mutation_rate)
 				{
-						var new_pick = dice.Next(logos.Count() - 1);
-						genetic_material[pos] = logos[new_pick];
+					var new_pick = dice.Next(logos.Count() - 1);
+					genetic_material[pos] = logos[new_pick];
 				}
 			}
 		}
 
 		public Genome(string solution_space)
 		{
-				logos = solution_space;
+			logos = solution_space;
 		}
-}
+	}
 
-
-public class Population
-{
+	public class Population
+	{
 		public List<Genome> all_genomes = new List<Genome>();
 		public string logos { get; set; }
 		public int dna_length { get; set; }
+		Random rnd = new Random();
 
 		public void init(int size)
 		{
-				for (int x = 0; x < size; x++)
-				{
-						Genome individual = new Genome(logos);
-						individual.init(dna_length);
-						all_genomes.Add(individual);
-				}
-
+			for (int x = 0; x < size; x++)
+			{
+				Genome individual = new Genome(logos);
+				individual.init(dna_length);
+				all_genomes.Add(individual);
+			}
 		}
 
 		public void evaluate(Func<Genome,int> fitness_function)
@@ -72,8 +71,7 @@ public class Population
 			all_genomes = all_genomes.OrderByDescending
 			(genome => genome.score).ToList();
 		}
-
-		
+			
 		public void mutate(int mutation_rate)
 		{
 			foreach(Genome individual in all_genomes)
@@ -82,49 +80,43 @@ public class Population
 			}
 		}
 
-		public void roulette_wheel_selection()
+		public void roulette_wheel_selection(int elitism)
 		{
 			List<Genome> wheel = new List<Genome>();
 			int size = all_genomes.Count();
 			for (int x = 0; x < size; x++)
 			{
-				for(int i = 0; i < all_genomes[x].score ; i++)
+				for(int i = 0; i <= all_genomes[x].score ; i++)
 				{
 					wheel.Add(all_genomes[x]);
 				}
 			}
-
-			all_genomes.Clear();
-			Random rnd = new Random();
-			for(int i = 0; i < size ; i++)
+				
+			for(int i = elitism; i < size ; i++)
 			{
 				var pick1 = rnd.Next(wheel.Count());
 				var pick2 = rnd.Next(wheel.Count());
 				var a = wheel[pick1];
 			 	var b = wheel[pick2];
 				Genome child = crossover(a,b); 
-				all_genomes.Add(child);
+				all_genomes[i] = child;
 			}
 		}
 
 		public Genome crossover(Genome a, Genome b)
 		{
-				var cut_pos = a.dice.Next(a.genetic_material.Count());
-				for (int x = cut_pos ; x < a.genetic_material.Count()-1; x++)
-				{
-					a.genetic_material[x] = b.genetic_material[x];
-				}
-				a.score = 0;
-
-				return a;
+			var cut_pos = a.dice.Next(a.genetic_material.Count());
+			for (int x = cut_pos ; x < a.genetic_material.Count()-1; x++)
+			{
+				a.genetic_material[x] = b.genetic_material[x];
+			}
+			a.score = 0;
+			return a;
 		}
-
 
 		public Population(string solution_space, int dna_length, int population_size)
 		{
-				logos = solution_space;
-				this.dna_length = dna_length;
+			logos = solution_space;
+			this.dna_length = dna_length;
 		}
-
 }
-
